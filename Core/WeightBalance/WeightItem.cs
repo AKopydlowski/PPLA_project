f00aeec6e5b325c6c76e489dbb9fb82c59317176
AKopydlowski <olekkopy@gmail.com>
2025-12-15 20:39:03 +0100

    Add WeightItem data model

---
+++ Core/WeightBalance/WeightItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPLA_project.Core.WeightBalance
+{
+    internal class WeightItem
+    {
+        public string Name { get; set; }
+        public double WeightKg { get; set; }
+        public double ArmMeters { get; set; }
+
+        public double Moment => WeightKg * ArmMeters;
+    }
+}
